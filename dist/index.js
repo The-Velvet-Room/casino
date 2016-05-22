d4b9c319d630f1357ed77bb0fee2c3fc91882604
Camtendo <ccrockrom@gmail.com>
2016-05-21 19:01:17 -0500

    More component stuff

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
+/*Horizon and RethinkDB code */
 var horizon = Horizon();
 horizon.onReady(function() {
     document.querySelector('h1').innerHTML = 'Bets go here fam! Rethink connection established!';
@@ -5,14 +6,82 @@ horizon.onReady(function() {
 
 horizon.connect();
 
+// CurrentBet Structure
+// startDate (in millis)
+// endTime (in millis)
+// competitors[] (0=red, 1=blue)
+// odds[] (0=red, 1=blue)
+
+const currentBet = horizon('current-bet');
+const allWagers = horizon('all-wagers');
+const users = horizon('betting-users');
+
+//User Structure
+// Username (Twitch)
+// Points
+// HighestPoints
+
+function placeBet(betIndex, amount, username) {
+    let bet = {
+        index: betIndex,
+        amount: amount,
+        username: username
+    }
+    
+    //No bet currently active
+    if(!currentBet) {
+        return;
+    }
+    
+    //Invalid betIndex
+    if(betIndex < 0 || betIndex > 1) {
+        return;
+    }
+    
+    //Invalid bet amount
+    if(amount <= 0) {
+        return;
+    }
+    
+    var now = new Date();  
+    //Bets locked
+    if(now.getTime() < currentBet.endTime) {
+        return;
+    }
+    
+    allWagers.store(bet);
+}
+
+//Bet structure
+// betIndex (0=red, 1=blue)
+// name
+// amount
+
+//TODO Use from Twitch
+function getProfileIcon(username) {
+    return '';
+}
+
+/*React and Components */
+var WagerInput = React.createClass({
+   displayName: 'WagerInput',
+   render: function() {
+       return React.createElement('input', {
+           placeholder: 'Enter your bet',
+           max: this.props.maxBet,
+           type: 'number'
+    })
+   }
+});
+
 var Wager = React.createClass({
     displayName: 'Wager',
     render: function() {
-        var icon = React.createElement('img', {className: 'wager-icon', src: this.props.icon});
+        var icon = React.createElement('img', {className: 'wager-icon', src: this.props.icon || 'https://static-cdn.jtvnw.net/jtv_user_pictures/xarth/404_user_300x300.png'});
         var amount = React.createElement('div', {className: 'wager-amount'}, this.props.amount);
-        var name = React.createElement('div', {className: 'wager-name'}, this.props.name);
+        var name = React.createElement('div', {className: 'wager-name col-md-4'}, this.props.name);
         
-        return React.createElement('div', {className: 'wager-row'}, icon, amount, name);
+        return React.createElement('div', {className: 'wager-row'}, icon, name, amount);
     }
 });
 
@@ -20,6 +89,7 @@ var WagerList = React.createClass({
    displayName: 'WagerList',
    render: function() {
        var list = this.props.wagers.map(function(wager) {
+           wager.key = wager.name+wager.amount;
            return React.createElement(Wager, wager);
        });  
        return React.createElement('div', null, list);
@@ -32,13 +102,26 @@ var defaultWagerList = [{
     name: 'Empty'
 },{
     icon: 'hue',
-    amount: 100,
+    amount: 123456789,
     name: 'bruh'
 }];
 
 window.onload = function () {
+    //Left Bet Ledger
     ReactDOM.render(
         React.createElement(WagerList, {wagers: defaultWagerList}),
         document.getElementById('bet-list-left')
     );
+    
+    //Right Bet Ledger
+    ReactDOM.render(
+        React.createElement(WagerList, {wagers: defaultWagerList}),
+        document.getElementById('bet-list-right')
+    );
+    
+    //Bet Box
+    ReactDOM.render(
+        React.createElement(WagerInput, {maxBet: 50}),
+        document.getElementById('bet-input-target')
+    )
 }
